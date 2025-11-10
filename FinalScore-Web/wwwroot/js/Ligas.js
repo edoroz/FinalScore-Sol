5bb616d535a607bc938095363311380d55a6ca42
Edgar Oroz <eoroz@indconsulting.com>
2025-11-10 09:57:26 -0700

    Comentaar función cargarDataTable()

--- FinalScore-Web/wwwroot/js/Ligas.js
+++ FinalScore-Web/wwwroot/js/Ligas.js
@@ -7,41 +7,42 @@ $(document).ready(function () {
     CargarLigas();
 });
 
-function cargarDataTable() {
-    dataTable = $('#tblLigas').dataTable({
-        "ajax": {
-            "url": "/admin/ligas/GetAll",
-            "type": "GET",
-            "dataType": "json"
-        },
-        "columns": [
-            {"data": "id",      "width": "05%"},
-            {"data": "Nombre",  "width": "20%"},
-            {"data": "Deporte", "width": "20%"},
-            {"data": "Pais",    "width": "20%"},
-            {"data": "Nivel",   "width": "20%"},
-            {
-                "data": "id",
-                "render": function (data) {
-                    return `
-                        <div class="text-center">
-                            <a href="/Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
-                            <i class="far fa-edit"> Editar
-                            </a>
-                            &nbsp;
-                            <div class="text-center">
-                            <a onClick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
-                            <i class="far fa-trash-alt"> Borrar
-                            </a>
-                        </div>
-                        `;
-                },
-                "width": "15%"
-            }
-        ],
-        "width"     : "100%"
-    });
-}
+//function cargarDataTable() {
+
+//    dataTable = $('#tblLigas').dataTable({
+//        "ajax": {
+//            "url": "/admin/ligas/GetAll",
+//            "type": "GET",
+//            "dataType": "json"
+//        },
+//        "columns": [
+//            {"data": "id",      "width": "05%"},
+//            {"data": "Nombre",  "width": "20%"},
+//            {"data": "Deporte", "width": "20%"},
+//            {"data": "Pais",    "width": "20%"},
+//            {"data": "Nivel",   "width": "20%"},
+//            {
+//                "data": "id",
+//                "render": function (data) {
+//                    return `
+//                        <div class="text-center">
+//                            <a href="/Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+//                            <i class="far fa-edit"> Editar
+//                            </a>
+//                            &nbsp;
+//                            <div class="text-center">
+//                            <a onClick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+//                            <i class="far fa-trash-alt"> Borrar
+//                            </a>
+//                        </div>
+//                        `;
+//                },
+//                "width": "15%"
+//            }
+//        ],
+//        "width"     : "100%"
+//    });
+//}
 
 function CargarLigas() {
 
